0c244b9728be7488a644799d9a440def501224fb
sami0567 <151971213+sami0567@users.noreply.github.com>
2024-01-04 03:22:22 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,77 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "clear.jpeg"
+about_me = "I'm Samiya Pathak, a first year CS major at Georgia Institute of Technology. I have an interest in UI/UX and I hope to gain CS realated work experience."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/samiya-pathak-5395342a1/"
+my_github_url = "https://github.com/sami0567"
+my_email_address = "sami0567p@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331", "MATH 1552", "CS 2050"], 
+    "names":["Intro to CS", "Intro to OOP", "Integral Calculus", "Discrete Mathematics"], 
+    "semester_taken":["1st", "2nd", "2nd", "1st"],
+    "skills":["Data Structures: Lists, Dictionaries, Recursion, Abstraction, Debugging", "Object Oriented Programming in Java", "Definite and indefinite integrals, Techniques of integration, Improper integrals", "Formal Proofs and Logic, Algorithm Analysis "],
+    }
+experience_data = {
+    "Director of Web Design at Alpha Phi":(["In charge of the official Alpha Phi website.  Working on adding elements to the website that make it more user-friendly and aesthetically pleasing."], "websitee.jpeg"),
+    "Marketing Team Member at GT HexLabs":(["Part of the marketing team for the nonprofit student organization, HexLabs, which organizes large-scale Hackathons."],"hex.jpeg"),
+    "GT Vertically Integrated Project: Brain Trauma Assessment Protocols":(["- Part of a research team that builds mobile applications for IOS and Android devices that collects and analyzes visual, auditory, and tactual input for people that suffered from Traumatic Brain Injuries.",
+                                                                                
+                                                                                "- Methods and technologies used in this project include Cloud Computing, User Interface Design, Multimedia Web Applications, Signal Processing, Databases, and Machine Learning."], "brain.jpeg"), 
+
+}
+
+projects_data = {
+    "Description": (" Used Python, Movie APIs, and the Streamlit software to show a list of movies I love, like, and dislike based on genre with included plot, trailer, and movie poster"),
+    "Link": ("https://movie-ratings-3sjew4zunrxhlf2cjtiumr.streamlit.app/"),
+}
+
+programming_data = {
+    "Python": 90,
+    "HTML": 90,
+    "Java": 0,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "HTML": "🖥️",
+    "Java": "🖲️",
+}
+spoken_icons = {"English": "🇺🇸",
+    "Hindi": "🇮🇳",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Hindi": "Fluent",
+    "Spanish": "Proficient",
+}
+leadership_data = {
+    "Registered EMT": (["Aided my community by attending calls twice a week from 7 pm-5 am and 24-hour weekend calls, some involving drug overdoses, broken limbs, and anaphylactic shock."], "emt.jpeg"),
+
+}
+activity_data={
+    "Intern at the Environmental Health Lab at Columbia University": (["Published an article about Superfund Sites which was the lab’s research focus. Worked on my own independent research project regarding the metal concentrations of the Hackensack River."], "lab.webp"),
+    "Leader or Sunrise Bergen County": (["Created and manage the hub’s Instagram account which has 600+ followers, has allowed more people to join the hub (110+ members) and has allowed us to collaborate and get support from former Congress Candidate Arati S. Kreibich for multiple projects such as county-wide protests which have gotten news-coverage from North Jersey News. Winner of the EPA’s Presidential Youth Environmental Award of 2022."], "sunrise.webp"),
+}
